2ab025a436673f5e7ba11f98bbd60f3e871f9aad
codeur5469 <87094276+codeur5469@users.noreply.github.com>
2026-04-18 21:15:22 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 from keep_alive import keepalive
+import asyncio
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -27,12 +28,22 @@ async def on_member_join(member):
     # Channel 2
     channel2 = member.guild.get_channel(1495055776216121425)
     if channel2:
-        message = f"Bienvenue {member.name} sur le serveur !"
+        message = f"Bienvenue {member.mention} sur le serveur !"
         try:
             file = discord.File("welcome.gif")
             await channel2.send(content=message, file=file)
         except Exception as e:
             print(f"Erreur channel2 : {e}")
+    
+    channel3 = member.guild.get_channel(1495120367365390456)
+    if channel3:
+        message = f"{member.mention}"
+        try:
+            msg = await channel3.send(content=message)
+            await asyncio.sleep(2)
+            await msg.delete()
+        except Exception as e:
+            print(f"Erreur channel3 : {e}")
 
 @bot.event
 async def on_member_remove(member):
