5bb7003fdc3b0cad6e84959719791d2750c5abc2
codeur5469 <87094276+codeur5469@users.noreply.github.com>
2026-06-18 18:50:30 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -16,16 +16,16 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_member_join(member):
     # Channel 2
-    channel2 = member.guild.get_channel(1495055776216121425)
+    channel2 = member.guild.get_channel(1517126171010007113)
     if channel2:
         message = f"Bienvenue {member.mention} sur le serveur !"
         try:
-            file = discord.File("welcome.gif")
+            file = discord.File("3.gif")
             await channel2.send(content=message, file=file)
         except Exception as e:
             print(f"Erreur channel2 : {e}")
     
-    channel3 = member.guild.get_channel(1495120367365390456)
+    channel3 = member.guild.get_channel(1517129461038977107)
     if channel3:
         message = f"{member.mention}"
         try:
@@ -34,6 +34,29 @@ async def on_member_join(member):
             await msg.delete()
         except Exception as e:
             print(f"Erreur channel3 : {e}")
+
+@bot.command
+async def test(member):
+    # Channel 2
+    channel2 = member.guild.get_channel(1517126171010007113)
+    if channel2:
+        message = f"Bienvenue {member.mention} sur le serveur !"
+        try:
+            file = discord.File("3.gif")
+            await channel2.send(content=message, file=file)
+        except Exception as e:
+            print(f"Erreur channel2 : {e}")
+    
+    channel3 = member.guild.get_channel(1517129461038977107)
+    if channel3:
+        message = f"{member.mention}"
+        try:
+            msg = await channel3.send(content=message)
+            await asyncio.sleep(2)
+            await msg.delete()
+        except Exception as e:
+            print(f"Erreur channel3 : {e}")
+
 
 keepalive()
 bot.run(TOKEN)
